e990cb58edf2c68bae05da8890c56898b62558ac
Alamin-22 <mdalaminmollick@gmail.com>
2024-10-11 00:49:09 +0600

    jaskldjla

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,14 @@
+import { DataTable } from "@/components/table/DataTable";
 import StatCard from "@/components/StatCard";
+import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { columns } from "@/components/table/columns";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const appointments = await getRecentAppointmentList();
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
       <header className="admin-header">
@@ -31,22 +36,25 @@ const AdminPage = () => {
         <section className="admin-stat">
           <StatCard
             type={"appointments"}
-            count={5}
+            count={appointments.scheduledCount}
             label="Schedule Appointments"
             icon="/assets/icons/appointments.svg"
           />
           <StatCard
             type={"pending"}
-            count={10}
+            count={appointments.pendingCount}
             label="Pending Appointments"
             icon="/assets/icons/pending.svg"
           />
           <StatCard
             type={"cancelled"}
-            count={2}
+            count={appointments.pendingCount}
             label="Canceled Appointments"
             icon="/assets/icons/cancelled.svg"
           />
+        </section>
+        <section>
+          <DataTable columns={columns} data={appointments.documents} />
         </section>
       </main>
     </div>
